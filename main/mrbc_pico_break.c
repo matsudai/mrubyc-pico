49b986415ae19572317b8e6ffa5dc8c22ce50c0a
matsudai <matsudai.social.1131@gmail.com>
2026-03-10 09:09:07 +0900

    Use task-switch-callback instead of tick-callback

--- main/mrbc_pico_break.c
+++ main/mrbc_pico_break.c
@@ -2,7 +2,7 @@
   @brief Break信号による再起動機能
 
   USB CDCのBreak信号を検出し，ウォッチドッグタイマによるソフトリセットを実行する．
-  Break信号の検出はTinyUSBのコールバックで行い，mrbc_tickコールバックでメインコンテキストからリブートを実行する．
+  Break信号の検出はTinyUSBのコールバックで行い，mrbc_runのタスクスイッチ時にメインコンテキストからリブートを実行する．
 */
 #include "mrbc_pico_break.h"
 #include "mrubyc.h"
@@ -24,21 +24,21 @@ void tud_cdc_send_break_cb(uint8_t itf, uint16_t duration_ms) {
 
 /** @brief Break信号によるソフトウェアリセット
 
-  mrbc_set_tick_callback()で登録するコールバック．
-  Break信号が検出された場合，1度だけ100ms後に再起動を予約する．
+  mrbc_set_task_switch_callback()で登録するコールバック．
+  Break信号が検出された場合，100ms後に再起動を予約する．
 */
 static void break_check(void) {
   if (break_received) {
-    mrbc_set_tick_callback(NULL);
+    mrbc_set_task_switch_callback(NULL);
     watchdog_reboot(0, 0, 100);
   }
 }
 
 /** @brief Break信号によるリブート機能の初期化
 
-  mrbc_tickコールバックにbreak_checkを登録する．
+  mrbc_runのタスクスイッチコールバックにbreak_checkを登録する．
 */
 void mrbc_pico_break_init(struct VM* vm) {
   (void)vm;
-  mrbc_set_tick_callback(break_check);
+  mrbc_set_task_switch_callback(break_check);
 }
